524da8d8024bc7d97f4f9283478facc98717494a
Legion <vashkevich.es@phystech.edu>
2022-12-20 12:42:40 +0300

    delete local directories

--- cmake-build-debug-coverage/GoogleTests/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ownstreamer/proga/C++/Parsers/GoogleTests/googletest
-# Build directory: /home/ownstreamer/proga/C++/Parsers/cmake-build-debug-coverage/GoogleTests/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug-coverage/GoogleTests/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ownstreamer/proga/C++/Parsers/GoogleTests/googletest/googletest
-# Build directory: /home/ownstreamer/proga/C++/Parsers/cmake-build-debug-coverage/GoogleTests/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
